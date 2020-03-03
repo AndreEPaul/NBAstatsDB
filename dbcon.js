2dc32e131a3fd2fc20cded61853540c5d92f6925
andpaul <andrepaul1993@gmail.com>
2020-03-03 12:48:00 -0800

    first update

--- dbcon.js
+++ dbcon.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_wolfordj',
-  password        : '7235',
-  database        : 'cs340_wolfordj'
+  user            : 'cs340_paula',
+  password        : '5033',
+  database        : 'cs340_paula'
 });
 module.exports.pool = pool;
