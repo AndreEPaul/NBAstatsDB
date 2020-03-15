b1ecd17564bf58ba15833ebd38db895437050be1
Andre <andrepaul1993@gmail.com>
2020-03-14 17:42:29 -0700

    delete m:m relationship

--- players_positions.js
+++ players_positions.js
@@ -40,6 +40,5 @@ module.exports = function(){
             }
         });
     });
-
     return router;
 }();
---
+++ public/deletePlayPos.js
@@ -0,0 +1,24 @@
+function deleteResult(plID, poID){
+
+    var table = document.getElementById("play_pos_table");
+    for (var row = 1;row <= (table.rows.length-1); row++) {
+        if((table.rows[row].cells[0].innerHTML == plID) && (table.rows[row].cells[1].innerHTML == poID)){
+            table.deleteRow(row);
+        }
+    }
+
+    var req = new XMLHttpRequest();
+    req.open("GET", "delete?id=" + keyID, true);
+
+    req.addEventListener("load",function(){
+        if(req.status >= 200 && req.status < 400){
+            console.log("success");
+        } else {
+            console.log("Error in network request: " + req.statusText);
+            if(req.statusText === "Not Found"){
+                alert("Error not found");
+            }
+        }
+    });
+    req.send(null);
+};
