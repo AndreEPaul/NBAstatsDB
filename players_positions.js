2c6cc1477c360b80e20ae55ceb7483827997f101
Andre <andrepaul1993@gmail.com>
2020-03-17 17:32:23 -0700

    new attempt for delete

--- players_positions.js
+++ players_positions.js
@@ -16,6 +16,7 @@ module.exports = function(){
     /*Display all players_positions */
     router.get('/', function(req, res){
         var context = {};
+        context.jsscripts = ["deleteplayers.js"];
         var mysql = req.app.get('mysql');
         getPlayersPositions(res, mysql, context, complete);
         function complete(){
@@ -39,6 +40,24 @@ module.exports = function(){
                 res.redirect('/players_positions');
             }
         });
+    });
+
+    router.delete('/plID/:plID/poID/:poID', function(req, res){
+        console.log(req);
+        console.log(req.params.plID);
+        console.log(req.params.poID);
+        var mysql = req.app.get('mysql');
+        var sql = "DELETE FROM Players_Positions WHERE plID = ? AND poID = ?";
+        var inserts = [req.params.plID, req.params.poID];
+        sql = mysql.pool.query(sql, inserts, function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.status(400);
+                res.end();
+            }else{
+                res.status(202).end();
+            }
+        })
     });
 
     return router;
